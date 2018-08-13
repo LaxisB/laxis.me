894aac6ad6aec5e8643db5cc6bdda52f704c6cd2
Lars Brix <lars.brix@vispiron.de>
2018-08-13 14:18:59 +0200

    add comment

--- src/home/index.tsx
+++ src/home/index.tsx
@@ -1,5 +1,9 @@
 import "./home.scss";
 
+/**
+ * add a href to the email anchor
+ * (ab)use the fact that innerText does not return hidden elements,
+ * since we have an invisible span in our html to block scrapers
+ */
 const mailEl: HTMLAnchorElement = document.querySelector(".links__row--email .links__name");
-
 mailEl.setAttribute("href", `mailto:${mailEl.innerText}`);
